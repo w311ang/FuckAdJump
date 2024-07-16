d7585f74f0eedfa0dfbfefc38ca1bb10525f97da
w311ang <30381083+w311ang@users.noreply.github.com>
2024-07-16 14:11:00 +0800

    Update XPosedMain.java

--- app/src/main/java/top/weixiansen574/fuckadjump/XPosedMain.java
+++ app/src/main/java/top/weixiansen574/fuckadjump/XPosedMain.java
@@ -1,13 +1,8 @@
 package top.weixiansen574.fuckadjump;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Handler;
-import android.os.Looper;
 import android.webkit.WebView;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -19,6 +14,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class XPosedMain implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        XposedBridge.log('h65');
         // 拦截Uri.parse方法
         XposedHelpers.findAndHookMethod(Uri.class, "parse", String.class, new XC_MethodHook() {
             @Override
@@ -26,6 +22,7 @@ public class XPosedMain implements IXposedHookLoadPackage {
                 super.beforeHookedMethod(param);
                 String arg = (String) param.args[0];
 
+                XposedBridge.log('hiii');
                 XposedBridge.log(arg);
 
                 if (arg == null) {
@@ -35,12 +32,12 @@ public class XPosedMain implements IXposedHookLoadPackage {
                     return;
                 }
                 if (arg.startsWith("openapp.jdmobile")) {
-                    XposedBridge.log("尝试跳转京东！");
-                    showDialog(param, (Activity) param.thisObject, "检测到跳转京东，是否继续？");
+                    XposedBridge.log("已拦截跳转京东！");
+                    param.args[0] = "";
                 }
                 if (arg.startsWith("tbopen")) {
-                    XposedBridge.log("尝试跳转淘宝！");
-                    showDialog(param, (Activity) param.thisObject, "检测到跳转淘宝，是否继续？");
+                    XposedBridge.log("已拦截跳转淘宝！");
+                    param.args[0] = "";
                 }
             }
         });
@@ -56,12 +53,12 @@ public class XPosedMain implements IXposedHookLoadPackage {
                     String uriString = uri.toString();
                     XposedBridge.log(uriString);
                     if (uriString.startsWith("openapp.jdmobile")) {
-                        XposedBridge.log("尝试跳转京东！");
-                        showDialog(param, (Activity) param.thisObject, "检测到跳转京东，是否继续？");
+                        XposedBridge.log("已拦截跳转京东！");
+                        param.setResult(null);
                     }
                     if (uriString.startsWith("tbopen")) {
-                        XposedBridge.log("尝试跳转淘宝！");
-                        showDialog(param, (Activity) param.thisObject, "检测到跳转淘宝，是否继续？");
+                        XposedBridge.log("已拦截跳转淘宝！");
+                        param.setResult(null);
                     }
                 }
             }
@@ -75,39 +72,13 @@ public class XPosedMain implements IXposedHookLoadPackage {
                 String url = (String) param.args[0];
                 XposedBridge.log(url);
                 if (url.startsWith("openapp.jdmobile")) {
-                    XposedBridge.log("尝试跳转京东！");
-                    showDialog(param, (Activity) param.thisObject, "检测到跳转京东，是否继续？");
+                    XposedBridge.log("已拦截跳转京东！");
+                    param.setResult(null);
                 }
                 if (url.startsWith("tbopen")) {
-                    XposedBridge.log("尝试跳转淘宝！");
-                    showDialog(param, (Activity) param.thisObject, "检测到跳转淘宝，是否继续？");
+                    XposedBridge.log("已拦截跳转淘宝！");
+                    param.setResult(null);
                 }
-            }
-        });
-    }
-
-    private void showDialog(final XC_MethodHook.MethodHookParam param, final Activity activity, String message) {
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-                new AlertDialog.Builder(activity)
-                        .setTitle("跳转提示")
-                        .setMessage(message)
-                        .setPositiveButton("允许", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        })
-                        .setNegativeButton("拒绝", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                param.setResult(null);
-                            }
-                        })
-                        .setCancelable(false)
-                        .show();
             }
         });
     }
