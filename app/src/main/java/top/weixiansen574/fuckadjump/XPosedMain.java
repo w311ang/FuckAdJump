6db4d2d29fc4d2e86e79eba580726ae95791b68e
w311ang <30381083+w311ang@users.noreply.github.com>
2024-07-16 13:44:55 +0800

    Update XPosedMain.java

--- app/src/main/java/top/weixiansen574/fuckadjump/XPosedMain.java
+++ app/src/main/java/top/weixiansen574/fuckadjump/XPosedMain.java
@@ -1,6 +1,9 @@
 package top.weixiansen574.fuckadjump;
 
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.webkit.WebView;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -11,7 +14,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class XPosedMain implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        XposedHelpers.findAndHookMethod(Uri.class,"parse",String.class, new XC_MethodHook() {
+        // 拦截Uri.parse方法
+        XposedHelpers.findAndHookMethod(Uri.class, "parse", String.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 super.beforeHookedMethod(param);
@@ -19,19 +23,59 @@ public class XPosedMain implements IXposedHookLoadPackage {
 
                 XposedBridge.log(arg);
 
-                if (arg == null){
+                if (arg == null) {
                     return;
                 }
-                if (arg.startsWith("http")){
+                if (arg.startsWith("http")) {
                     return;
                 }
-                if (arg.startsWith("openapp.jdmobile")){
+                if (arg.startsWith("openapp.jdmobile")) {
                     XposedBridge.log("已拦截跳转京东！");
-                    param.args[0]= "";
+                    param.args[0] = "";
                 }
-                if (arg.startsWith("tbopen")){
+                if (arg.startsWith("tbopen")) {
                     XposedBridge.log("已拦截跳转淘宝！");
-                    param.args[0]= "";
+                    param.args[0] = "";
+                }
+            }
+        });
+
+        // 拦截Activity的startActivity方法
+        XposedHelpers.findAndHookMethod("android.app.Activity", loadPackageParam.classLoader, "startActivity", Intent.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                Intent intent = (Intent) param.args[0];
+                Uri uri = intent.getData();
+                if (uri != null) {
+                    String uriString = uri.toString();
+                    XposedBridge.log(uriString);
+                    if (uriString.startsWith("openapp.jdmobile")) {
+                        XposedBridge.log("已拦截跳转京东！");
+                        param.setResult(null);
+                    }
+                    if (uriString.startsWith("tbopen")) {
+                        XposedBridge.log("已拦截跳转淘宝！");
+                        param.setResult(null);
+                    }
+                }
+            }
+        });
+
+        // 拦截WebView的loadUrl方法
+        XposedHelpers.findAndHookMethod(WebView.class, "loadUrl", String.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                String url = (String) param.args[0];
+                XposedBridge.log(url);
+                if (url.startsWith("openapp.jdmobile")) {
+                    XposedBridge.log("已拦截跳转京东！");
+                    param.setResult(null);
+                }
+                if (url.startsWith("tbopen")) {
+                    XposedBridge.log("已拦截跳转淘宝！");
+                    param.setResult(null);
                 }
             }
         });
